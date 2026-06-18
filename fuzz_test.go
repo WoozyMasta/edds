3ca86def770d437b58dafd4a5e183b01f0b82437
Maxim Levchenko <kagbe.leviy@gmail.com>
2026-06-18 04:59:18 +0300

    test: add fuzz

---
+++ fuzz_test.go
@@ -0,0 +1,67 @@
+package edds
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func FuzzReadConfig(f *testing.F) {
+	f.Add([]byte{})
+	f.Add([]byte("DDS "))
+	f.Add([]byte{
+		'D', 'D', 'S', ' ',
+		124, 0, 0, 0,
+	})
+
+	f.Fuzz(func(t *testing.T, data []byte) {
+		if len(data) > 4096 {
+			t.Skip()
+		}
+
+		path := filepath.Join(t.TempDir(), "input.edds")
+		if err := os.WriteFile(path, data, 0o600); err != nil {
+			t.Fatalf("write fuzz input: %v", err)
+		}
+
+		_, _ = ReadConfig(path)
+	})
+}
+
+func FuzzDecompressBlock(f *testing.F) {
+	f.Add(BlockMagicCOPY, []byte{}, 0)
+	f.Add(BlockMagicCOPY, []byte{1, 2, 3}, 3)
+	f.Add(BlockMagicCOPY, []byte{1, 2, 3}, 2)
+	f.Add(BlockMagicLZ4, []byte{}, 16)
+	f.Add("BAD!", []byte{1, 2, 3, 4}, 4)
+
+	raw := make([]byte, 4096)
+	for i := range raw {
+		raw[i] = byte(i / 64)
+	}
+	block, err := compressBlockWithOptions(raw, normalizedCompressionOptions{
+		mode:      CompressionLZ4,
+		minRatio:  1.0 / 0.85,
+		chunkSize: ChunkSize,
+	})
+	if err == nil {
+		f.Add(block.Magic, block.Data, len(raw))
+	}
+
+	f.Fuzz(func(t *testing.T, magic string, data []byte, expectedSize int) {
+		if len(magic) > 16 || len(data) > 64*1024 {
+			t.Skip()
+		}
+		if expectedSize < 0 || expectedSize > 64*1024 {
+			t.Skip()
+		}
+
+		block := &Block{
+			Magic: magic,
+			Size:  int32(len(data)), //nolint:gosec // bounded above.
+			Data:  data,
+		}
+
+		_, _ = decompressBlock(block, expectedSize)
+	})
+}
